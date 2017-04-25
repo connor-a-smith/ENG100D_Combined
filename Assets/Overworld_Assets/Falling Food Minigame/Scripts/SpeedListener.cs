cc158a5177431a5a3801e62191ab1f4dbcd51599
Connor Smith <c3smith@ucsd.edu>
2017-04-24 20:10:57 -0700

    Removed all redundant files... but tons of missing scripts

--- Assets/Overworld_Assets/Falling Food Minigame/Scripts/SpeedListener.cs
+++
@@ -1,9 +0,0 @@
-﻿/// <summary>
-/// Speed listener interface for objects that should change with scroll speed.
-/// </summary>
-public interface SpeedListener {
-
-  //method that will be called by SpeedChanger, updates the speed
-  void updateScrollSpeed(float speed);
-
-}
